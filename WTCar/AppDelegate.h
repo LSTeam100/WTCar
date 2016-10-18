cf16ab1b1ab62bb98b5817057d45af27ab7e875d
duanran <drandgis@163.com>
2016-10-18 14:04:54 +0800

    Initial Commit

---
+++ WTCar/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WTCar
+//
+//  Created by apple on 2016/10/18.
+//  Copyright © 2016年 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WTCar/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WTCar
+//
+//  Created by apple on 2016/10/18.
+//  Copyright © 2016年 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
