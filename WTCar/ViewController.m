4e862b1155f602f562bc4eede2cfbb59741dc237
MacBookPro <MAC@MacBookProdeMacBook-Pro.local>
2016-10-21 20:52:00 +0800

    suTest

--- WTCar/ViewController.m
+++ WTCar/ViewController.m
@@ -16,6 +16,8 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    UIView *myView = [[UIView alloc]init];
+    myView.backgroundColor =[UIColor blackColor];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
